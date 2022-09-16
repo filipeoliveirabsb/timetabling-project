83d1368a29d469fcb968bee7c6c031384432cfe1
F3288636 Filipe Oliveira da Silva <filipe_oliveira@bb.com.br>
2022-09-16 10:19:26 -0300

    ajustes de nomes

---
+++ genetic.py
@@ -0,0 +1,27 @@
+class GeneticAlgorithm:
+    print("Main")
+    # def crossover:
+
+    #def mutation:
+
+    #def evolution:
+
+class Chromosome:
+    print("chromosomes")
+
+    # def genes:
+
+    # def entropy:
+
+    # def fitness:
+
+    # def get_timetable:
+
+class Population:
+    print("Main")
+
+    # def get_chromosomes:
+
+    # def sort:
+
+
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from ruamel.yaml import YAML
 
 # *------------------ RULES ---------------------*
 # the intervals between schedules are 3 days long
-# optimal number of hours in month / agent=160h
-# max hours in month / agent = 168h
-# min agents / day in module = 2
-# max agents / day in module = 4 (team lenght) 
+# optimal number of hours in month / employee=160h
+# max hours in month / employee = 168h
+# min employees / day in module = 2
+# max employees / day in module = 4 (team lenght) 
 
 # A solução deve:
 # 1) gerar as escalas com o algoritmo heuristico 
@@ -19,82 +19,82 @@ from ruamel.yaml import YAML
 #       ...
 # 2) na geração da escala, 
 #    2.1) considerar as ausências de cada time e 
-#           que há um time (4 agentes) por dia
+#           que há um time (4 empregados) por dia
 #    2.2) gerar randomicamente pelos arranjos da 
 #           ordem dos times    
-# 3) somar as horas dos agentes no mês incluir na
-#    coluna "total horas" no time_table
+# 3) somar as horas dos empregados no mês incluir 
+#    na coluna "total horas" no time_table
 # 4) utilizar o algoritmo genetico pra encontrar a 
 #    melhor escala, com setTime para medir o tempo 
 #    de execução
 
-def get_employee_absences(team_agent):
+def get_employee_absences(team_employee):
     
     absences = data["scale_constraints"].index()
 
     return absences
 
 # generate time_table scale with heuristic algorithm
-def get_time_tables(data, poss):
-    time_table = pd.DataFrame(index=data["agents"], columns=data["days"])
+def get_schedule_tables(data, poss):
+    schedule_table = pd.DataFrame(index=data["employees"], columns=data["days"])
 
     modules = data["modules"]
     days = data["days"]
-    agents = data["agents"]
+    employees = data["employees"]
     schedule = data["team_schedule"]
     
-    #shape = (len(modules) * len(days) * len(agents), len(schedule))
+    #shape = (len(modules) * len(days) * len(employees), len(schedule))
     #genes = np.zeros(shape, dtype=np.int32)
 
     (first, second, third, fourth) = poss
         
     for k in range(len(schedule)):
     
-        (team, team_agent, _) = schedule[k]
+        (team, team_employee, _) = schedule[k]
 
         if team == first:
 
             (scale_days) = data["scale_days"][0]
 
-            create_scheduling(time_table, days, agents, team_agent, scale_days)
+            create_scheduling(schedule_table, days, employees, team_employee, scale_days)
 
         if team == second:
 
             (scale_days) = data["scale_days"][1]
 
-            create_scheduling(time_table, days, agents, team_agent, scale_days)
+            create_scheduling(schedule_table, days, employees, team_employee, scale_days)
 
         if team == third:
 
             (scale_days) = data["scale_days"][2]
 
-            create_scheduling(time_table, days, agents, team_agent, scale_days)                       
+            create_scheduling(schedule_table, days, employees, team_employee, scale_days)                       
                     
         if team == fourth:
 
             (scale_days) = data["scale_days"][3]
 
-            create_scheduling(time_table, days, agents, team_agent, scale_days) 
+            create_scheduling(schedule_table, days, employees, team_employee, scale_days) 
 
-    return time_table
+    return schedule_table
 
 
 
-def create_scheduling(time_table, days, agents, team_agent, scale_days):
+def create_scheduling(schedule_table, days, employees, team_employee, scale_days):
     for day in range(len(days)):
-        for agent in range(len(agents)):
+        for employee in range(len(employees)):
                     
-            if (agents[agent] == team_agent and day in scale_days):
+            if (employees[employee] == team_employee and day in scale_days):
                 
                 # employee absences
-                """ employee_absences = get_employee_absences(team_agent)
+                """ employee_absences = get_employee_absences(team_employee)
                 for ab in range(len(employee_absences)):
                     (absences_type, absences_days) = employee_absences[ab]
 
                 if day in range(absences_days):
-                    time_table.iloc[agent, day] = absences_type  
+                    time_table.iloc[employee, day] = absences_type  
                 else: """
-                time_table.iloc[agent, day] = 24
+                schedule_table.iloc[employee, day] = 24
 
 
 if __name__ == '__main__':
@@ -125,6 +125,6 @@ if __name__ == '__main__':
         #print(last_team)
         # descarta que o último do mês anterior seja o primeiro do mês atual
         if last_team != first:
-            print(get_time_tables(data, poss))
+            print(get_schedule_tables(data, poss))
             print("fim")
 
